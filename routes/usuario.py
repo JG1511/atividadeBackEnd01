24f6b421dae8e9b141e9a4b3c5695aad3cae3f38
JG1511 <joaotsi2024@gmail.com>
2025-05-01 22:47:25 -0300

    implementação dos html

--- routes/usuario.py
+++ routes/usuario.py
@@ -3,10 +3,12 @@ from database.usuario import USUARIO
 
 usuario_route = Blueprint('usuario',__name__)
 
-@usuario_route.route('/', methods = ['GET', 'POST'])
+@usuario_route.route('/login', methods = ['GET', 'POST'])
 def login_usuario():
     if request.method == 'POST':
-        return
+        return validacaoLogin
+    else:
+        pass 
     
 def validacaoLogin():
     pass
