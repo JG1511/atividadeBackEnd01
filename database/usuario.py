c503f3c3f7be67c8e588d1aafd91a5a2e65db466
JG1511 <joaotsi2024@gmail.com>
2025-05-01 22:26:23 -0300

    criação do BD de verificação do login

---
+++ database/usuario.py
@@ -0,0 +1,6 @@
+#BD fake para validações de login
+usuario = [
+    {"id": 1, "email": "guilhermebr@gmail.com", "senha":123},
+    {"id": 2, "email": "daniel34@gmail.com", "senha":123},
+    {"id": 3, "email": "cerola@gmail.com", "senha":123}
+]
