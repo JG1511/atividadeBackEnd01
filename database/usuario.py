a05883b4a2d1918006f6222f8e946faa473f463f
JG1511 <joaotsi2024@gmail.com>
2025-05-02 01:07:19 -0300

    lógica para lista de música

--- database/usuario.py
+++ database/usuario.py
@@ -1,6 +1,6 @@
 #BD fake para validações de login
 USUARIO = [
-    {"id": 1, "email": "guilhermebr@gmail.com", "senha":123},
-    {"id": 2, "email": "daniel34@gmail.com", "senha":123},
-    {"id": 3, "email": "cerola@gmail.com", "senha":123}
+    {"id": 1,"nome": "Guilherme" ,"email": "guilhermebr@gmail.com", "senha":123},
+    {"id": 2,"nome": "Daniel","email": "daniel34@gmail.com", "senha":123},
+    {"id": 3,"nome": "Cerol" ,"email": "cerola@gmail.com", "senha":123}
 ]
--- routes/usuario.py
+++ routes/usuario.py
@@ -1,22 +1,30 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template,redirect,url_for
 from database.usuario import USUARIO
 
 usuario_route = Blueprint('usuario', __name__)
 
 @usuario_route.route('/login', methods=['GET', 'POST'])
 def login_usuario():
+    # Se a requisição for do método post, ele irá chamar a função de validação do login
     if request.method == 'POST':
-        return validacaoLogin()
+        return validacaoLogin() 
     else:
         return render_template('forms.html')
 
 def validacaoLogin():
+    # email e senha vão pegar os dados do forms, por isso a utilização do ".form.get"
     email = request.form.get('email')
     senha = request.form.get('senha')
-
+    # laço de repetição para percorrer a lista de discionário e comparar com os dados armazenados do forms e armazenar o nome
     for usuario in USUARIO:
         if usuario['email'] == email and usuario['senha'] == int(senha):
-            return render_template('teste.html', usuarioLogado=usuario)
+            nome = usuario['nome']
+            # O redirect além de rederecionar também manda o nome para '/perfil/<nome>''
+            return redirect(url_for('usuario.perfil', nome = nome))
     
     
     return render_template('forms.html', erro="Email ou senha incorretos")
+
+@usuario_route.route('/perfil/<nome>')
+def perfil(nome):
+    return render_template('lista_musica.html',nome = nome)
