78d76dc285b3938558c160ac5df1a5396f044e6f
JG1511 <joaotsi2024@gmail.com>
2025-05-01 22:37:28 -0300

    implementando a validação do login

--- routes/usuario.py
+++ routes/usuario.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from database.usuario import USUARIO
 
 usuario_route = Blueprint('usuario',__name__)
 
-@usuario_route.route('/')
+@usuario_route.route('/', methods = ['GET', 'POST'])
 def login_usuario():
+    if request.method == 'POST':
+        return
+    
+def validacaoLogin():
     pass
